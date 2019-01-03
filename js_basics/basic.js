283410855e2307ebdcb66ec2565ad509baea3a67
ericCartmanie <yshi20@students.claremontmckenna.edu>
2019-01-03 11:28:04 +0800

    new

--- js_basics/basic.js
+++ js_basics/basic.js
@@ -1,3 +1,13 @@
 var myName = "Yingqi Shi"
 var cool = myName.charAt(0)
 console.log(cool)
+
+
+// global context variable grade
+var grade = "F";
+if(grade === "A" || grade==="B" || grade==="C" || grade==="D"){
+    console.log("you pass");
+}
+else{
+    console.log("You Failed !!");
+};
